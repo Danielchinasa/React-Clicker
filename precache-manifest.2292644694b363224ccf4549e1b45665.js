be7b7c91703bb525ca432aa6212714dd58b2d5bc
Daniel Okoro <danielochinasa@gmail.com>
2020-06-03 13:20:43 +0100

    Updates

---
+++ precache-manifest.2292644694b363224ccf4549e1b45665.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533b3e324f43942dd230d6455b97be5c",
+    "url": "/React-Clicker/index.html"
+  },
+  {
+    "revision": "95e145579882bdf3d033",
+    "url": "/React-Clicker/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "45a72e37d9adc24d8c28",
+    "url": "/React-Clicker/static/css/main.b01570af.chunk.css"
+  },
+  {
+    "revision": "95e145579882bdf3d033",
+    "url": "/React-Clicker/static/js/2.cc6d2fcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Clicker/static/js/2.cc6d2fcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a72e37d9adc24d8c28",
+    "url": "/React-Clicker/static/js/main.8c2cf7ac.chunk.js"
+  },
+  {
+    "revision": "df35ce1dbb6d1f055acf",
+    "url": "/React-Clicker/static/js/runtime-main.0757dc43.js"
+  }
+]);
